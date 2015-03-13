56e1c20edb9ca479f65cbdb5710472453b3c85cf
Alvaro Pereyra <alvaro@xendacentral.com>
2015-03-13 14:06:40 -0600

    Adds more info in credentials call

--- lib/omniauth/strategies/codepicnic.rb
+++ lib/omniauth/strategies/codepicnic.rb
@@ -15,9 +15,16 @@ module OmniAuth
 
       info do
         {
-          :email => raw_info["email"]
+          :email => raw_info["email"],
+          :first_name => raw_info["first_name"],
+          :last_name => raw_info["last_name"],
+          :avatar_url => raw_info["avatar_url"],
           # and anything else you want to return to your API consumers
         }
+      end
+
+      extra do
+        {'access_token' => access_token}
       end
 
       def raw_info
